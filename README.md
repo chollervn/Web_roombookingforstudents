2166936c0a60ea91849cf6a9a1abf5e42633b86b
Bảy cỏ <anhquandz311@gmail.com>
2025-11-30 15:43:26 +0700

    29/11/2025: làm gọn code #4

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web_roombookingforstudents
-Trang web hỗ trợ cho thuê và quản lí phòng trọ sinh viên
--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Java\jdk-21
-set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d D:\Pjxc\OOP_BTL
-
-call mvnw.cmd spring-boot:run
-
